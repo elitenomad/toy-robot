9a8cb7f78e3258a63abc2fc7bfa6b9bff6fd65fd
pranava s balugari <stalin.pranava@gmail.com>
2020-08-06 15:05:47 +1000

    Toy::Robot Initial commit - Created using bundle gem

---
+++ lib/toy/robot.rb
@@ -0,0 +1,8 @@
+require "toy/robot/version"
+
+module Toy
+  module Robot
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/toy/robot_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Toy::Robot do
+  it "has a version number" do
+    expect(Toy::Robot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
